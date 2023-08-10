3aa968b129c98f6237cf27ad9b4aeaa7594f2f60
solusmax <solusmaxim@gmail.com>
2023-08-10 13:19:40 +0300

    Add Vite Image Optimizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import browserslistToEsbuild from 'browserslist-to-esbuild'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteImageOptimizer(),
+  ],
   build: {
     target: browserslistToEsbuild(),
   },
