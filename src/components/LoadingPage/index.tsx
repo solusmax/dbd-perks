50e744db788f1171963aae8a44f9869545f504db
solusmax <solusmaxim@gmail.com>
2023-09-13 16:44:52 +0300

    Redraw loading spinner

--- src/components/LoadingPage/index.tsx
+++ src/components/LoadingPage/index.tsx
@@ -3,7 +3,7 @@ import './styles.scss'
 export default function LoadingPage(): React.JSX.Element {
   return (
     <div className="loaging-page">
-      <div className="loading-page__spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+      <span className="loading-page__spinner"></span>
     </div>
   )
 }
