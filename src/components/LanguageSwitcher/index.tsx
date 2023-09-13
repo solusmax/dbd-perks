fd5716b2158d4d01f8289d676deed8bd6810502b
solusmax <solusmaxim@gmail.com>
2023-09-13 16:03:03 +0300

    Add language switcher

--- src/components/LanguageSwitcher/index.tsx
+++ src/components/LanguageSwitcher/index.tsx
@@ -1,15 +1,51 @@
+import './styles.scss';
 import { ComponentProps } from '../../types';
-import languages from '../../data/languages.json'
-import { changeLanguage } from '../../utils';
+import languagesJson from '../../data/languages.json'
+import { useState } from 'react';
+import { changeLanguage, getCurrentLanguage } from '../../utils';
+import Select from 'react-select';
 
 type LanguageSwitcherProps = ComponentProps;
 
+type Language = {
+  value: string,
+  label: string
+};
+
+const languages: Language[] = languagesJson;
+
+const getDefaultLanguage = () => {
+  return languages.find((language) => language.value === getCurrentLanguage()) ?? languages[0];
+}
+
 export default function LanguageSwitcher({ className }: LanguageSwitcherProps = {}): React.JSX.Element {
+  const [selectedOption, setSelectedOption] = useState<Language>(getDefaultLanguage());
+
+  const handleChange = (language: Language | null) => {
+    if (!language) {
+      setSelectedOption(getDefaultLanguage());
+      changeLanguage(getDefaultLanguage().value);
+      return;
+    }
+
+    setSelectedOption(language);
+    changeLanguage(language.value);
+  }
+
   return (
     <div className={`language-switcher ${className}`}>
-      {languages.map((language) => {
-        return <button key={language} onClick={() => changeLanguage(language)}>{language.toUpperCase()}</button>
-      })}
+      <Select
+        className='language-switcher__select'
+        classNamePrefix='language-switcher'
+        isSearchable={false}
+        components={{
+          DropdownIndicator: () => null,
+          IndicatorSeparator: () => null
+        }}
+        defaultValue={selectedOption}
+        onChange={(newValue) => handleChange(newValue)}
+        options={languages}
+      />
     </div>
   )
 }
--- src/utils/app.ts
+++ src/utils/app.ts
@@ -3,3 +3,7 @@ import i18next from 'i18next';
 export const changeLanguage = (language: string) => {
   void i18next.changeLanguage(language);
 };
+
+export const getCurrentLanguage = () => {
+  return i18next.language;
+}
