d97820449699346400373429af5cd17f7c266f9b
solusmax <solusmaxim@gmail.com>
2023-10-02 15:25:31 +0300

    Add import sorter and format config files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,33 +2,34 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended-type-checked',
-    'plugin:@typescript-eslint/stylistic-type-checked',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: [
-    'dist',
-    '.eslintrc.cjs',
-    'postcss.config.js'
-  ],
-  parser: '@typescript-eslint/parser',
+  ignorePatterns: ["dist", ".eslintrc.cjs", "postcss.config.js"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json', './tsconfig.node.json'],
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
     tsconfigRootDir: __dirname,
-   },
-  plugins: ['react-refresh'],
+  },
+  plugins: ["react-refresh"],
   rules: {
-    'quotes': ['warn', 'single'],
-    'react-refresh/only-export-components': [
-      'warn',
+    quotes: ["warn", "single"],
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
-    '@typescript-eslint/strict-boolean-expressions': 'warn',
-    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+    "@typescript-eslint/strict-boolean-expressions": "warn",
+    "@typescript-eslint/consistent-type-definitions": ["error", "type"],
   },
-}
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import browserslistToEsbuild from 'browserslist-to-esbuild'
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import browserslistToEsbuild from 'browserslist-to-esbuild';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [
-    react(),
-    ViteImageOptimizer(),
-  ],
+  plugins: [react(), ViteImageOptimizer()],
   build: {
     target: browserslistToEsbuild(),
   },
-})
+});
