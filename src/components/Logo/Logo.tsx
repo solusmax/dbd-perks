dcc6ae6ea2be8234eeaf34751ac1c6c2469cb3a1
solusmax <solusmaxim@gmail.com>
2023-10-03 14:12:55 +0300

    Fix unintentional Cyrillic use

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,3 +1,5 @@
+import { useTranslation } from 'react-i18next';
+
 import { CustomComponentProps } from '@/types';
 
 import './styles.scss';
@@ -7,9 +9,15 @@ type LogoProps = CustomComponentProps;
 export default function Logo({
   className = '',
 }: LogoProps = {}): React.JSX.Element {
+  const { t } = useTranslation();
+
   return (
     <div className={`logo ${className}`}>
-      <img className="logo__img" src="img/logo.svg" alt="Логотип DBD Perks." />
+      <img
+        className="logo__img"
+        src="img/logo.svg"
+        alt={t('dbd-perks-logo', { ns: 'app' })}
+      />
       <span className="logo__title">DBD Perks</span>
     </div>
   );
