d4989ca052467b277c42b4809fd2aea91cb77a98
solusmax <solusmaxim@gmail.com>
2023-10-17 18:30:09 +0300

    Add preloading of UI images

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,24 @@
 import react from '@vitejs/plugin-react-swc';
 import browserslistToEsbuild from 'browserslist-to-esbuild';
 import path from 'path';
+import UnpluginInjectPreload from 'unplugin-inject-preload/vite';
 import { defineConfig } from 'vite';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react(), ViteImageOptimizer()],
+  plugins: [
+    react(),
+    ViteImageOptimizer(),
+    UnpluginInjectPreload({
+      files: [
+        {
+          entryMatch: /\.(png|jpg|jpeg|webp|gif|svg)$/i,
+        },
+      ],
+    }),
+  ],
   build: {
     target: browserslistToEsbuild(),
   },
